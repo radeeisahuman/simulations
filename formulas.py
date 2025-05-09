4c6622bed94a9f9655484e64a048d7877094807e
Radee <radeehasadream@gmail.com>
2025-05-09 10:02:50 +0600

    Added the files

---
+++ formulas.py
@@ -0,0 +1,7 @@
+gravity = 9.81
+
+def calculate_velocity(acceleration: float, time: float):
+    return acceleration * time
+
+def distance_travelled(velocity: float, time: float):
+    return velocity * time
---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame as pg
+import formulas
+
+def main():
+    pg.init()
+    screen = pg.display.set_mode((1600, 780))
+    pg.display.set_caption('Gravity Sim')
+    clock = pg.time.Clock()
+    circle = pg.draw.circle(screen, (255,255,0), (800, 100), 50)
+    rectangle = pg.draw.rect(screen, (0, 255, 0), (0, 600, 1600, 180))
+    running = True
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running=False
+        
+        pg.display.flip()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
